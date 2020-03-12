2de1968ac2ec65970c1d3a85850d9dc56764a609
unknown <amin7@ukr.net>
2020-03-12 20:06:45 +0200

    probe vis

--- frontend/spiffs/www/js/controls.js
+++ frontend/spiffs/www/js/controls.js
@@ -1,5 +1,4 @@
 var interval_position = -1;
-var controls_panel_inited=false;
 const cmd_SetPosition000='G92 X0 Y0 Z0';
 const cmd_SetPositionZ0='G92 Z0';
 const cmd_AutoHome='G28';
@@ -14,13 +13,43 @@ const cmd_ReportSDPrintStatus ='M27';
 const cmd_LinearMoveIdle='G0';
 const cmd_GetCurrentPosition='M114';
 
+var smoothieProbe = new SmoothieChart({
+    millisPerPixel: 200,
+    maxValueScale: 1.1,
+    minValueScale: 1.1,
+    enableDpiScaling: false,
+    interpolation: 'step',
+    horizontalLines:[ { value: 0, color: '#000000', lineWidth: 1 } ],
+    grid: {
+        fillStyle: '#ffffff',
+        strokeStyle: 'rgba(128,128,128,0.5)',
+        verticalSections: 5.,
+        millisPerLine: 0,
+        borderVisible: false
+    },
+    labels: {
+        fillStyle: '#000000',
+        precision: 1
+    }
+});
+var smoothieProbeSeries = new TimeSeries();
 
 function init_controls_panel() {    
     document.getElementById('control_xy_velocity').value =  config.feedrate.move_xy;
     document.getElementById('control_z_velocity').value =  config.feedrate.move_z;
-    controls_panel_inited=true;
+    smoothieProbe.addTimeSeries(smoothieProbeSeries, {
+        lineWidth: 1,
+        strokeStyle: '#808080',
+        fillStyle: 'rgba(128,128,128,0.3)'
+    });
+    smoothieProbe.streamTo(document.getElementById("id_probeGraph") );
+    smoothieProbe.stop();
     get_Position();
+    // setInterval(function() {
+    //   smoothieProbeSeries.append(new Date().getTime(), Math.random()-0.5);
+    // }, 1000);
 }
+
 
 function controls_UpdateStatus(status){
     if((typeof status.position)!='undefined'){
@@ -28,8 +57,10 @@ function controls_UpdateStatus(status){
             document.getElementById('control_x_position').innerHTML = status.position.X;
         if((typeof status.position.Y)!='undefined')
             document.getElementById('control_y_position').innerHTML = status.position.Y;
-        if((typeof status.position.Z)!='undefined')
+        if((typeof status.position.Z)!='undefined'){
             document.getElementById('control_z_position').innerHTML = status.position.Z;
+            smoothieProbeSeries.append(new Date().getTime(), status.position.Z);         
+        }
     }
 }
 
@@ -113,9 +144,12 @@ function controls_ProbeTargetMultiple(fActive) {
     ptm_active=fActive;
     if(fActive){
         controls_ProbeTarget(document.getElementById('id_ProbeDistance').value,controls_ProbeTargetMultiple_callback)
+        smoothieProbeSeries.clear();
+        smoothieProbe.start();
     }else{
         clear_command_list();
         controls_gotoZHop(config.probe.multiple.hop);
+        smoothieProbe.stop();
     }
 }
 
